d5635ce29ca34185d5bcdd83ff3798c4c710bde8
beepboopblap <89974500+beepboopblap@users.noreply.github.com>
2022-03-17 14:48:50 +0800

    Create password-generator.py

---
+++ password-generator.py
@@ -0,0 +1,14 @@
+import random as rdm 
+
+alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alp_cap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'rR', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+num = ['1','2','3','4','5','6','7','8','9', '0']
+random_password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'rR', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','1','2','3','4','5','6','7','8','9', '0']
+
+rdm.shuffle(random_password)
+random_password = (','.join(random_password))
+random_password = random_password.replace(',','')
+
+print("Welcome!")
+print("Here is your generated password: ",random_password)
+
